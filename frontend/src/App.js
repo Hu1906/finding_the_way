9a1d6e14fb45fc58bdea9316d42b9d79b3b5930a
Nguyễn Kim Huy <nkhuy05@gmail.com>
2025-12-22 02:48:27 +0700

    chore: fix frontend linh tinh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -101,7 +101,7 @@ function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <h1>Tìm Đường Đi - Hai Bà Trưng, Hà Nội</h1>
+        <h1>Tìm Đường Đi - Hà Nội</h1>
 
         <AlgorithmSelector
           selectedAlgorithm={selectedAlgorithm}
